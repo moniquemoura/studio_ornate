68bcadb511df02fd1384e5f69e65c6f64f0ed70a
daniloxd2022 <113191711+daniloxd2022@users.noreply.github.com>
2023-01-31 17:43:27 -0300

    up do ClassName

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,5 +1,6 @@
 import './footer.css'
 import {Link} from "react-router-dom";
+import React from 'react';
 
 export default function Footer() {
     return (
--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,5 +1,6 @@
 import './navbar.css'
 import {Link} from "react-router-dom";
+import React from 'react';
 
 export default function Navbar() {
     return (
